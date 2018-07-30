6bc91c5392a124abb99eb7cd77b64f54ca2de42a
Andre Cavalcante <andreluiz_pinho@hotmail.com>
2018-07-30 19:31:12 -0300

    create migrate to models post and currency

--- db/migrate/20180730220727_create_posts.rb
+++ db/migrate/20180730220727_create_posts.rb
@@ -3,7 +3,6 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.integer :amount
       t.integer :ease
-      t.references :currency, foreign_key: true
       t.references :user, foreign_key: true
 
       t.timestamps
