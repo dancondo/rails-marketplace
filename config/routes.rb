8668d05b0d22c9b215d9a1e546893f07a68d07c5
Andre Cavalcante <andreluiz_pinho@hotmail.com>
2018-07-31 18:08:55 -0300

    create a wrong controller for orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'orders/show'
+  get 'orders/create'
+  get 'orders/index'
+  get 'orders/show'
+  get 'orders/new'
   resources :posts
   devise_for :users
   resources :users, only: :show
