7343b4f63d07c6c3e025f8c1f5f9b027e2c1b396
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2018-08-02 17:06:31 -0300

    Create a seed.rb file that makes new users orders, posts, and currencies with a fictinal rate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_02_165538) do
+ActiveRecord::Schema.define(version: 2018_08_02_190704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2018_08_02_165538) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "amount"
+    t.float "amount"
     t.bigint "user_id"
     t.bigint "post_id"
     t.datetime "created_at", null: false
