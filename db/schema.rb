7203be7d813a63494b92e49d05ce9f1c8f3d945b
Andre Cavalcante <andreluiz_pinho@hotmail.com>
2018-08-02 15:07:27 -0300

    erasing ease from earth

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_08_02_165801) do
+ActiveRecord::Schema.define(version: 2018_08_02_165538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,13 +36,12 @@ ActiveRecord::Schema.define(version: 2018_08_02_165801) do
 
   create_table "posts", force: :cascade do |t|
     t.float "amount"
-    t.integer "amount"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "currency_id"
-    t.integer "price"
     t.datetime "duration"
+    t.integer "price"
     t.index ["currency_id"], name: "index_posts_on_currency_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
