8cc5f99d01bbe6a8a1536dbed2bbdd5e1f1e8b94
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2018-08-03 10:55:07 -0300

    minnor changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,5 @@
 import "bootstrap";
-
-const desired_amount = document.querySelector('#desired_amount');
-const value = document.querySelector('#total_value');
-const rate = document.querySelector('#rate').innerHTML;
-
-const calculate = () => {
-  value.innerHTML = desired_amount.value * rate
-}
-
-document.addEventListener('keyup', calculate);
+import realTimeAmount from './real_time_amount.js'
+import higherOrLower from './higher_or_lower'
+realTimeAmount();
+higherOrLower();
